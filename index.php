4b16f470cbd04e7307e1e3886d6fa349902e5d46
sachin-darekar <108718627+sachin-darekar@users.noreply.github.com>
2026-06-17 12:32:59 +0530

    Refactor index.php to improve readability

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations Azure CODE  Pieline DEMO !</h1>
+        <h1>Congratulations Azure CODE  Pieline DEMO From Git Hub !</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
